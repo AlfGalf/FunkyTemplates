f344ca89e52942eb0d5d4d66dc72946acb0d7463
Alfie Richards <main@alfierichards.com>
2022-02-17 13:55:36 +0000

    Arguments and pattern matching

--- README.md
+++ README.md
@@ -1,27 +1,43 @@
 # Funky Templates
 
-## TODO
+## TODOs:
 
 ### Parser:
 
-- Patterns DONE
-- Logic operators DONE
-- Other number operators ?
-- Full document DONE
-- Multiline string DONE
-- String interpolation DONE
-- Multi line string interpolation DONE
-- Lambdas DONE
+- [x] Patterns
+- [x] Logic operators
+- [x] Other number operators
+- [x] Full document
+- [x] Multiline string
+- [x] String interpolation
+- [x] Multi line string interpolation
+- [x] Lambdas
 
-### Interpreter
+### Interpreter:
 
-- All
-- Try to get some basic programs working for demo?
+- [x] Basic arithmetic and string building DONE
+- [x] Basic function calling and frames
+- [x] Arguments and pattern matching
+- [ ] Lists
+- [ ] Guards
+- [ ] Booleans
+- [ ] Token hashtable?
+- [ ] Better errors (`@L @R`)
+- [ ] Builtin function
+    - [ ] List
+    - [ ] Map
+    - [ ] Filter
+    - [ ] Any
+    - [ ] All
+    - [ ] Fold
 
-### API
+### API:
 
-- Look into API design patterns in Rust
-- Define API
-- API for AST printing?
-    - Good for demo of progress
+- [x] Look into API design patterns in Rust
+- [x] Define API
+- [ ] API for AST printing?
+    - [ ] Good for demo of progress
+- [ ] Adding arguments
+- [ ] Flesh out return types
+- [ ] Adding extra builtin functions
 
--- src/interpreter/mod.rs
+++ src/interpreter/mod.rs
@@ -1,4 +1,5 @@
-use std::any::Any;
+use std::cell::RefCell;
+use std::collections::hash_map::Entry;
 use std::collections::HashMap;
 use std::ops::Add;
 
@@ -9,39 +10,64 @@ use crate::data_types::*;
 
 mod test;
 
+/// Frame for holding the environment in an execution of a program
+#[derive(Clone)]
 struct Frame {
-    frame: HashMap<String, Expr>,
-    next: Option<Box<Frame>>,
+    frame: HashMap<String, InterpretVal>,
+    next: Option<RefCell<Box<Frame>>>,
 }
 
 impl Frame {
-    fn new(t: &Template) -> Self {
+    fn new() -> Self {
         Self {
-            frame: t.env.clone(),
+            frame: HashMap::new(),
             next: None,
         }
     }
 
-    fn add(&mut self, name: String, expr: &Expr) {
-        self.frame.insert(name, expr.clone());
+    fn from_template(t: &Template) -> Self {
+        Self {
+            frame: t
+                .env
+                .iter()
+                .map(|(a, b)| (a.clone(), InterpretVal::Function(b.clone())))
+                .collect(),
+            next: None,
+        }
     }
 
-    fn find(&self, name: &str) -> Result<&Expr, InterpretError> {
-        self.frame
-            .get(name)
-            .ok_or_else(|| InterpretError::new("Could not find variable."))
+    fn add_val(&mut self, name: String, expr: &InterpretVal) -> Result<(), InterpretError> {
+        if let Entry::Vacant(e) = self.frame.entry(name) {
+            e.insert(expr.clone());
+            Ok(())
+        } else {
+            Err(InterpretError::new(
+                "Multiple variables within the same frame.",
+            ))
+        }
     }
-}
 
-fn print_type_of<T>(_: &T) -> String {
-    std::any::type_name::<T>().to_string()
+    fn find(&self, name: &str) -> Result<InterpretVal, InterpretError> {
+        if let Some(r) = self.frame.get(name) {
+            Ok(r.clone())
+        } else if let Some(n) = &self.next {
+            n.borrow().find(name)
+        } else {
+            Err(InterpretError::new("Cannot find value."))
+        }
+    }
 }
 
 pub fn interpret(temp: &Template, name: &str) -> Result<ReturnVal, InterpretError> {
     let res = {
-        if let Some(func) = temp.env.get(name) {
-            if let Expr::Function(f) = func {
-                interpret_function(f, &mut Frame::new(temp))
+        let frame = Frame::from_template(temp);
+        if let Ok(func) = frame.find(name) {
+            if let InterpretVal::Function(p) = func {
+                interpret_function(
+                    &p,
+                    &mut Frame::from_template(temp),
+                    InterpretVal::Tuple(vec![]),
+                )
             } else {
                 panic!("Should be impossible to have top level expr with non function expr");
             }
@@ -68,8 +94,8 @@ fn interpret_recurse(expr: &Expr, env: &mut Frame) -> Result<InterpretVal, Inter
         Expr::Number(n) => Ok(InterpretVal::Int(*n)),
         Expr::Unary(o, e) => match o {
             UnaryOp::Not => {
-                let res = interpret_recurse(e, env);
-                if let Ok(InterpretVal::Bool(e)) = res {
+                let res = interpret_recurse(e, env)?;
+                if let InterpretVal::Bool(e) = res {
                     Ok(InterpretVal::Bool(!e))
                 } else {
                     Err(InterpretError::new(
@@ -77,19 +103,30 @@ fn interpret_recurse(expr: &Expr, env: &mut Frame) -> Result<InterpretVal, Inter
                     ))
                 }
             }
+            UnaryOp::Neg => {
+                let res = interpret_recurse(e, env)?;
+                if let InterpretVal::Int(i) = res {
+                    Ok(InterpretVal::Int(-i))
+                } else {
+                    Err(InterpretError::new(
+                        "Tried to apply '-' to a non int value.",
+                    ))
+                }
+            }
         },
         Expr::Function(p) => Ok(InterpretVal::Function(p.clone())),
-        Expr::FuncCall(f, _) => {
+        Expr::FuncCall(f, a) => {
             let val = interpret_recurse(f, env)?;
+            let arg = interpret_recurse(&Expr::Tuple(a.clone()), env)?;
             if let InterpretVal::Function(p) = val {
-                interpret_function(&p, env)
+                interpret_function(&p, env, arg)
             } else {
                 Err(InterpretError::new(
                     "Called something that was not a function",
                 ))
             }
         }
-        Expr::Var(s) => interpret_recurse(&env.find(s)?.clone(), env),
+        Expr::Var(s) => env.find(s).clone(),
         Expr::InterpolationString(vs) => Ok(InterpretVal::String(
             vs.iter()
                 .map(|p| match p {
@@ -99,8 +136,15 @@ fn interpret_recurse(expr: &Expr, env: &mut Frame) -> Result<InterpretVal, Inter
                 .fold_ok(String::new(), |s, p| s.add(p.as_str()))?,
         )),
         Expr::Op(l, o, r) => eval_op(l, o, r, env),
+        Expr::Tuple(v) => Ok(InterpretVal::Tuple(
+            v.iter()
+                .map(|e| interpret_recurse(e, env))
+                .collect::<Result<Vec<InterpretVal>, InterpretError>>()?,
+        )),
 
-        _ => Err(InterpretError::new("Unrecognised node type.")),
+        e => Err(InterpretError::new(
+            format!("Unrecognised node type {:?}.", e).as_str(),
+        )),
     }
 }
 
@@ -130,22 +174,120 @@ fn eval_op(
                 }
             }
             t => Err(InterpretError::new(
-                format!("Add operator not defined for {}.", print_type_of(&t)).as_str(),
+                format!("Add operator not defined for {:?}.", t).as_str(),
+            )),
+        },
+        Opcode::Sub => {
+            if let (InterpretVal::Int(l), InterpretVal::Int(r)) = (left, right) {
+                Ok(InterpretVal::Int(l - r))
+            } else {
+                Err(InterpretError::new("Cannot subtract non string."))
+            }
+        }
+        Opcode::Mul => match (left, right) {
+            (InterpretVal::Int(l), InterpretVal::Int(r)) => Ok(InterpretVal::Int(l * r)),
+            (InterpretVal::String(l), InterpretVal::Int(r)) => {
+                if r >= 0 {
+                    Ok(InterpretVal::String(l.repeat(r as usize)))
+                } else {
+                    Err(InterpretError::new(
+                        "Cannot repeat a string a negative number of times",
+                    ))
+                }
+            }
+            (l, r) => Err(InterpretError::new(
+                format!("Invalid operators for multiplication: {:?} * {:?}", l, r).as_str(),
+            )),
+        },
+        Opcode::Div => match (left, right) {
+            (InterpretVal::Int(l), InterpretVal::Int(r)) => Ok(InterpretVal::Int(l / r)),
+            (l, r) => Err(InterpretError::new(
+                format!("Invalid operators for division: {:?} * {:?}", l, r).as_str(),
             )),
         },
         _ => todo!("Other op types"),
     }
 }
 
-fn interpret_function(func: &[Pattern], env: &mut Frame) -> Result<InterpretVal, InterpretError> {
-    // TODO: Match pattern
-    // TODO: use params
+fn pattern_match(
+    param: Box<Expr>,
+    arg: InterpretVal,
+    env: &mut Frame,
+) -> Result<Option<Frame>, InterpretError> {
+    let mut res = Frame::new();
 
-    // for p in func {
-    // }
+    let mut stack = vec![(param, arg.unwrap_tuple())];
+    //     if let InterpretVal::Tuple(s) = arg {
+    //         if s.len() == 1 {
+    //             vec![(&param, s.first().unwrap().clone())]
+    //         } else {
+    //             vec![(&param, InterpretVal::Tuple(s))]
+    //         }
+    //     } else {
+    //         vec![(&param, arg)]
+    //     }
+    // };
 
-    let pat = func
-        .first()
-        .ok_or_else(|| InterpretError::new("Cannot find applicable pattern"))?;
-    interpret_recurse(&pat.result, env)
+    while !stack.is_empty() {
+        let (cur_param, cur_arg) = {
+            let (p1, c1) = stack.pop().unwrap();
+            (p1.clone().unwrap_tuple(), c1.unwrap_tuple())
+        };
+
+        match cur_param {
+            Expr::Var(s) => {
+                res.add_val(s.clone(), &cur_arg)?;
+            }
+            Expr::Tuple(s) => {
+                if let InterpretVal::Tuple(v) = cur_arg {
+                    if s.len() == v.len() {
+                        for (p, a) in s
+                            .into_iter()
+                            .zip(v)
+                            .collect::<Vec<(Box<Expr>, InterpretVal)>>()
+                        {
+                            stack.push((p, a).clone())
+                        }
+                    }
+                } else {
+                    return Ok(None);
+                }
+            }
+            e => {
+                let res = interpret_recurse(&e, env)?;
+                if !(res == cur_arg) {
+                    return Ok(None);
+                }
+            }
+        }
+    }
+    res.next = Some(RefCell::new(Box::new(env.clone())));
+
+    Ok(Some(res))
+}
+
+fn interpret_function(
+    func: &[Pattern],
+    env: &mut Frame,
+    arg: InterpretVal,
+) -> Result<InterpretVal, InterpretError> {
+    for p in func {
+        if let Some(mut r) = pattern_match(p.start.clone(), arg.clone(), env)? {
+            return interpret_recurse(&*p.result, &mut r);
+        }
+    }
+
+    Err(InterpretError::new("Cannot find applicable pattern"))
+}
+
+fn unwrap_expr(e: Box<Expr>) -> Box<Expr> {
+    if let Expr::Tuple(s) = *e {
+        if s.len() == 1 {
+            s[0].clone()
+        } else {
+            Box::new(Expr::Tuple(s))
+        }
+    } else {
+        e
+    }
 }
