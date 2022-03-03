14fb428ab557237e6e4e399360e0a978997d8ed5
Alfie Richards <main@alfierichards.com>
2022-03-03 13:25:42 +0000

    Add comments and documentation

--- build.rs
+++ build.rs
@@ -1,5 +1,6 @@
 extern crate lalrpop;
 
+/// Generates the parser and lexer code from the `.lalrpop` file at compile time
 fn main() {
-    lalrpop::process_root().unwrap();
+  lalrpop::process_root().unwrap();
 }
